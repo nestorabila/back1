98ee7eac0fb73d18b25e7d61dba9a0c60e58f1bf
Nestor Abila <abilaf354@gmail.com>
2025-08-05 21:49:55 -0400

    otro cambio mas

--- src/config/database.js
+++ src/config/database.js
@@ -6,12 +6,19 @@ dotenv.config();
 const sequelize= new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PSW, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_TYPE,
+    port: 5432,
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }, logging: false,
+    dialectOptions: {
+        ssl: {  
+            require: true,
+            rejectUnauthorized: false,
+        }
+    },
 });
 sequelize.authenticate().then(() => {
     console.log('Conectado a la base de datos!');
