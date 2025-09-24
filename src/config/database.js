dd3b78b1dd6fbd1014083c678f3b182fc64b8b72
nestorabila <abilaf354@gmail.com>
2025-09-23 20:52:15 -0400

    cambios paras posgrs5

--- src/config/database.js
+++ src/config/database.js
@@ -1,31 +1,29 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const initModels = require('../modelos/init-models')
+const { Sequelize } = require('sequelize');
+const initModels = require('../modelos/init-models');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize= new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PSW, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_TYPE,
-    port: 5432,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    },
+    logging: false,
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
-    }, logging: false,
-    dialectOptions: {
-        ssl: {  
-            require: true,
-            rejectUnauthorized: false,
-        }
     },
 });
-sequelize.authenticate().then(() => {
-    console.log('Conectado a la base de datos!');
-}).catch((err) => {
-        console.log(err);
-});
 
+sequelize.authenticate()
+    .then(() => console.log('Conectado a la base de datos!'))
+    .catch(err => console.error('Error al conectar:', err));
 
-const db = initModels(sequelize); 
+const db = initModels(sequelize);
 module.exports = { sequelize, db };
